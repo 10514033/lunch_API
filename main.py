b61eef69e97e5d2edebba837665fa4f6bd38ab06
ZIV <59049004+10514033@users.noreply.github.com>
2024-08-05 14:39:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,14 @@
-import pymongo
-from flask import *
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-client = pymongo.MongoClient(
-    "mongodb+srv://ziv:nm123456@ziv.ljfz9lq.mongodb.net/?retryWrites=true&w=majority&appName=ziv")
-print("success")
-app = Flask(
-    __name__,
-    static_folder="public",
-    static_url_path="/"
-)
-app.secret_key = "key22"
-
-db = client.lunch
+app = FastAPI()
 
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+class User(BaseModel):
+    username: str
+    password: str
 
 
-@app.route("/user", methods=["POST"])
-def user():
-    name = request.form.get("username")
-    session["username"] = name
-    passwd = request.form.get("password")
-    collection = db.user
-
-    collection.insert_one({
-        "username": name,
-        "password": passwd
-    })
-
-    return render_template("user.html", username=name)
-
-
-@app.route("/yc")
-def yc():
-
-    return "沒ㄐㄐ"
-
-
+@app.get("/yc")
+async def login():
+    return {"沒ㄐㄐ"}
